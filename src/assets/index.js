ce3d60a7b79fb6f942bdc8e2cfb1a7782190c4a9
unknown <mobifone@DESKTOP-S59VM5C.mobifone.vn>
2023-11-20 11:27:22 +0700

    update layout

---
+++ src/assets/index.js
@@ -0,0 +1,64 @@
+import logo from './logo.svg';
+import backend from './backend.png';
+import creator from './creator.png';
+import mobile from './mobile.png';
+import web from './web.png';
+import github from './github.png';
+import menu from './menu.svg';
+import close from './close.svg';
+
+import css from './tech/css.png';
+import docker from './tech/docker.png';
+import figma from './tech/figma.png';
+import git from './tech/git.png';
+import html from './tech/html.png';
+import javascript from './tech/javascript.png';
+import mongodb from './tech/mongodb.png';
+import nodejs from './tech/nodejs.png';
+import reactjs from './tech/reactjs.png';
+import redux from './tech/redux.png';
+import tailwind from './tech/tailwind.png';
+import typescript from './tech/typescript.png';
+import threejs from './tech/threejs.svg';
+
+import mobifone from './company/mobifone.png';
+import houze from './company/houze.png';
+import md24 from './company/md24.png';
+import namtech from './company/namtech.png';
+
+import conecta from './conecta-background.png';
+import houzeBG from './houze-background.png';
+import houzeApp from './houze-app.jpeg';
+import logoUser from './logo-user.jpg';
+
+export {
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  docker,
+  figma,
+  git,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  redux,
+  tailwind,
+  typescript,
+  threejs,
+  mobifone,
+  houze,
+  md24,
+  namtech,
+  conecta,
+  houzeBG,
+  houzeApp,
+  logoUser,
+};
